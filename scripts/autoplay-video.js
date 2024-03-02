06bf5769b90a65a63ccfedc937d48c2f4c385b8a
Juan Pablo Jiménez <juanpajimenezp2710@gmail.com>
2024-03-02 13:02:12 -0600

    Update index.html

---
+++ scripts/autoplay-video.js
@@ -0,0 +1,8 @@
+//String base para el debug 
+let srtDebug = '';
+
+//1.Saber el tamano del viewport
+let altoBrowser = window.innerHeight;
+srtDebug += "Alto del browser: "+altoBrowser+"px"
+
+alert(srtDebug);
