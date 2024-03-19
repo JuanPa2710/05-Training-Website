75c6c9ebc29618ef7d84c0664167a575d221b379
Juan Pablo Jiménez <juanpajimenezp2710@gmail.com>
2024-03-18 23:44:46 -0600

    Add section testimonies

--- scripts/app.js
+++ scripts/app.js
@@ -1,17 +1,40 @@
+//Variables del menu desplegable
 let menuDesplegable = document.getElementById('menu-desplegable');
 let desplegado = false;
 
+//Variables de las flechas del carrusel
+let flechaIzq = document.getElementById('button-left');
+let flechaDer = document.getElementById('button-right');
+let carrusel = document.getElementById('carrusel')
+let posicion = 0;
+
+//Procedimiento que despliega el menu de opciones
 function desplegarMenu() {
     if(desplegado == false) {
         menuDesplegable.style.visibility = 'visible';
         menuDesplegable.style.opacity = '1';
-        menuDesplegable.style.transform = 'translateX(0%)'
+        menuDesplegable.style.transform = 'translateX(0%)';
         desplegado = true;
     }
     else {
         menuDesplegable.style.visibility = 'hidden';
         menuDesplegable.style.opacity = '0';
-        menuDesplegable.style.transform = 'translateX(-100%)'
+        menuDesplegable.style.transform = 'translateX(-100%)';
         desplegado = false;
     }
 };
+
+//Procedimiento que mueve las imagenes del carrusel de derecha a izquierda
+function moverIzquierda() {
+    if(posicion != 0) {
+        posicion += 100;
+        carrusel.style.transform = "translateX("+posicion+"%)";        
+    }
+}
+
+function moverDerecha() {
+    if(posicion != -500) {
+        posicion -= 100;
+        carrusel.style.transform = "translateX("+posicion+"%)";        
+    }
+}
